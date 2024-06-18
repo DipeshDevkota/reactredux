e0e0f8e7c368a09b036bc3770c8279a7a62faa64
Dipesh Devkota <devkotadipesh12@gmail.com>
2024-06-18 23:58:28 +0545

    first commit

---
+++ redux_toolkit/src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+const App = () => {
+  return (
+    <>
+      <h1>REACT_REDUX_TOOLKIT</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
